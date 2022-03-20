8b18e33f839f18b8012a1099319b2958a52f6be1
EldinHb <eldin.33@outlook.com>
2022-03-20 13:28:08 +0100

    removed dist from tracking

--- packages/components/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './buttons';
-export * from './labels';
-//# sourceMappingURL=index.d.ts.map
--- packages/components/dist/labels/label.d.ts
+++
@@ -1,7 +0,0 @@
-declare type Props = {
-    children?: string | never[];
-    className?: string;
-};
-export declare const Label: (props: Props) => JSX.Element;
-export {};
-//# sourceMappingURL=label.d.ts.map
